e429eab1e467e37b34cb372482400da8a802ea2a
SapinSobre <nat.dao3@gmail.com>
2019-05-28 15:41:22 +0200

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using OuKonMange3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace OuKonMange3
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);    
+
+            IDatabaseInitializer<BddContext> init = new InitOuKonMange3();
+            Database.SetInitializer(init);
+            init.InitializeDatabase(new BddContext());            
+        }
+    }
+}
